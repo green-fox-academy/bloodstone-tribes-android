21ef05b1f6f152f4e224a021bc831acd44f8e495
georgezsiga <georgezsiga@gmail.com>
2017-06-11 22:49:27 +0200

    get shared preference and print to screen

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.greenfox.tribesoflagopusandroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String message = preferences.getString("Username", "");
+        textView.setText(message);
     }
     
     public void sendMessage(View view) {
