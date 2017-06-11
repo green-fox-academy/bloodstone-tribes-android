eada2573b1dea97c190c89be5ca97d5dbf160bab
georgezsiga <georgezsiga@gmail.com>
2017-06-11 23:27:52 +0200

    changes to test

--- app/src/test/java/com/greenfox/tribesoflagopusandroid/LoginTest.java
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/LoginTest.java
@@ -1,5 +1,9 @@
 package com.greenfox.tribesoflagopusandroid;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +12,15 @@ import static org.junit.Assert.*;
  * Created by georgezsiga on 6/11/17.
  */
 public class LoginTest {
-    @Test
-    public void addUserInfoToPreferences() throws Exception {
 
-    }
 
     @Test
     public void addUsername() throws Exception {
-
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        String username = "Test";
+        String message = preferences.getString("Username", "");
+        assertEquals(username, message);
     }
 
     @Test
