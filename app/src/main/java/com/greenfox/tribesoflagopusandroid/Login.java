e46a5f96a7de074834a35ea6770de960588d12bc
georgezsiga <georgezsiga@gmail.com>
2017-06-11 22:50:10 +0200

    create save username to shared preferences method

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/Login.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/Login.java
@@ -1,9 +1,13 @@
 package com.greenfox.tribesoflagopusandroid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -14,7 +18,17 @@ public class Login extends AppCompatActivity {
     }
 
     public void login(View view) {
+        addUsernameToPreferences();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void addUsernameToPreferences() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String username = editText.getText().toString();
+        editor.putString("Username", username);
+        editor.apply();
     }
 }
