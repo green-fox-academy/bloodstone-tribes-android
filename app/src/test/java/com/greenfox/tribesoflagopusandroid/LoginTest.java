bee65b9422452a2ec961d846c70d1fd6e50c1089
georgezsiga <georgezsiga@gmail.com>
2017-06-11 23:02:17 +0200

    add test for login page

---
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/LoginTest.java
@@ -0,0 +1,26 @@
+package com.greenfox.tribesoflagopusandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by georgezsiga on 6/11/17.
+ */
+public class LoginTest {
+    @Test
+    public void addUserInfoToPreferences() throws Exception {
+
+    }
+
+    @Test
+    public void addUsername() throws Exception {
+
+    }
+
+    @Test
+    public void addPassword() throws Exception {
+
+    }
+
+}
