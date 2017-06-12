77e301666cbaed6715062ee03ec623279957715b
georgezsiga <georgezsiga@gmail.com>
2017-06-12 12:16:25 +0200

    Add 2 tests

--- app/src/test/java/com/greenfox/tribesoflagopusandroid/LoginTest.java
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/LoginTest.java
@@ -8,28 +8,39 @@ import android.widget.EditText;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by georgezsiga on 6/11/17.
  */
+@Config(constants = BuildConfig.class)
+@RunWith(RobolectricTestRunner.class)
 public class LoginTest {
 
     Login login;
 
+    @Before
+    public void setup() {
+        login = Robolectric.setupActivity(Login.class);
+    }
 
     @Test
     public void addUsername() throws Exception {
-        Button button = (Button) login.findViewById(R.id.button2);
         EditText username = (EditText) login.findViewById(R.id.editText3);
-        button.performClick();
-        assertEquals(username, login.preferences.getString("Username", ""));
+        login.addUsername();
+        assertEquals(username.getText().toString(), login.preferences.getString("Username", ""));
     }
 
     @Test
     public void addPassword() throws Exception {
-
+        EditText password = (EditText) login.findViewById(R.id.editText2);
+        login.addPassword();
+        assertEquals(password.getText().toString(), login.preferences.getString("Password", ""));
     }
 
 }
