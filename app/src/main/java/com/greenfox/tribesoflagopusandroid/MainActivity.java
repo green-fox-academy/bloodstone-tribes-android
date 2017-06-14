283cd3139d3f1e2f25f0adbdc8dfa4302e4dca76
georgezsiga <georgezsiga@gmail.com>
2017-06-14 16:18:25 +0200

    Add menu button and toast message

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.greenfox.tribesoflagopusandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,17 +17,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "greenfox.com.tribesoflagopus.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.refreshing:
+                Toast.makeText(MainActivity.this, "Refreshing", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
     public void sendMessage(View view) {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
