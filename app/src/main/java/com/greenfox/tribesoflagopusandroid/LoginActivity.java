77281049763f837879e84270f0587fc1cbf6cb5a
Viktor Hegyi <viktorhegyi@gmail.com>
2017-06-14 17:43:11 +0200

    add background image

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
@@ -17,9 +17,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
         editor = preferences.edit();
+
     }
 
     public void login(View view) {
--- app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.greenfox.tribesoflagopusandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
     }
 
     public void sendMessage(View view) {
