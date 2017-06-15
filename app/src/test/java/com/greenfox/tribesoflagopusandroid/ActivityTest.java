16c8b742cdf9eeb4a9752686e050874730505102
georgezsiga <georgezsiga@gmail.com>
2017-06-15 13:36:11 +0200

    delete unused code

--- app/src/test/java/com/greenfox/tribesoflagopusandroid/ActivityTest.java
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/ActivityTest.java
@@ -1,17 +1,11 @@
 package com.greenfox.tribesoflagopusandroid;
 
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import static org.junit.Assert.assertEquals;
 
 @Config(constants = BuildConfig.class)
 @RunWith(RobolectricTestRunner.class)
@@ -24,14 +18,5 @@ public class ActivityTest {
         activity = Robolectric.setupActivity(MainActivity.class);
     }
 
-    @Test
-    public void wildButtonTest() throws Exception {
-
-        Button button = (Button) activity.findViewById(R.id.button);
-        TextView result = (TextView) activity.findViewById(R.id.textView2);
-        EditText edit = (EditText) activity.findViewById(R.id.editText);
-        button.performClick();
-        assertEquals(result.getText().toString(), edit.getText().toString());
-    }
 }
 
