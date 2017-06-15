f5a3bfc54840f13f7bb1dbf011468d71ef87f5ee
georgezsiga <georgezsiga@gmail.com>
2017-06-15 15:00:59 +0200

    test

--- app/src/test/java/com/greenfox/tribesoflagopusandroid/ActivityTest.java
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/ActivityTest.java
@@ -7,11 +7,18 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
+
 @Config(constants = BuildConfig.class)
 @RunWith(RobolectricTestRunner.class)
 public class ActivityTest {
 
     MainActivity activity;
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
 
 }
 
