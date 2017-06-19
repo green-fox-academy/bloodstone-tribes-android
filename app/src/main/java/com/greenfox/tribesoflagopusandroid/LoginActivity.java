e47e87aac175fdf244b3805468a4b10599c72a69
georgezsiga <georgezsiga@gmail.com>
2017-06-19 16:43:58 +0200

    add checkfieldsnotempty method

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
@@ -5,6 +5,9 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -39,38 +42,56 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        addUserInfoToPreferences();
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        checkFieldsNotEmpty();
+//        addUserInfoToPreferences();
 
-        LoginService service = ServiceFactory.createMockService(LoginService.class, "https://tribes-of-lagopus.herokuapp.com/");
-        service.loginWithUser("username", "password").enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                User user = response.body();
-            }
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Toast.makeText(LoginActivity.this, "error :(", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
     }
 
-    protected void addUserInfoToPreferences() {
-        addUsername();
-        addPassword();
-    }
-
-    protected void addUsername() {
+    private void checkFieldsNotEmpty() {
         EditText editText = (EditText) findViewById(R.id.editText3);
         String username = editText.getText().toString();
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        String password = editText2.getText().toString();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            Toast toast = Toast.makeText(LoginActivity.this, "Please fill in all the fields", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        } else {
+            LoginService service = ServiceFactory.createMockService(LoginService.class, "https://tribes-of-lagopus.herokuapp.com/");
+            service.loginWithUser("username", "password").enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    User user = response.body();
+                    addUserInfoToPreferences(user.getUsername(), user.getPassword());
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    Toast.makeText(LoginActivity.this, "error getting login information from server", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    protected void addUserInfoToPreferences(String username, String password) {
+        addUsername(username);
+        addPassword(password);
+    }
+
+    protected void addUsername(String username) {
+//        EditText editText = (EditText) findViewById(R.id.editText3);
+//        String username = editText.getText().toString();
         editor.putString(USERNAME, username);
         editor.apply();
     }
 
-    protected void addPassword() {
-        EditText editText = (EditText) findViewById(R.id.editText2);
-        String password = editText.getText().toString();
+    protected void addPassword(String password) {
+//        EditText editText = (EditText) findViewById(R.id.editText2);
+//        String password = editText.getText().toString();
         editor.putString(PASSWORD, password);
         editor.apply();
     }
