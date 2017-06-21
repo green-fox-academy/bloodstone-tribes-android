fc790799c3b0ee4d33800841eb7e5f2990a581ea
Reka Giday <giday.r@gmail.com>
2017-06-21 14:16:59 +0200

    indentation fixed and some buildings added to the mock list

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/MockApiService.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/MockApiService.java
@@ -8,6 +8,8 @@ import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Troop;
 import com.greenfox.tribesoflagopusandroid.api.model.gameobject.User;
 import com.greenfox.tribesoflagopusandroid.api.model.response.BuildingsResponse;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import retrofit2.Call;
@@ -24,9 +26,12 @@ public class MockApiService implements ApiService {
     private long id = 1;
     private String name = "kingdomname";
     private long idOfUser = 1;
-    private List<Building> buildings = null;
-    private List<Resource> resources = null;
-    private List<Troop> troops = null;
+    private Building farm1 = new Building(1, "farm", 1, 2);
+    private Building mine1 = new Building(1, "mine", 1, 2);
+    private Building townhall = new Building(1, "townhall", 1, 10);
+    private List<Building> buildings = new ArrayList<>(Arrays.asList(farm1, mine1, townhall));
+    private List<Resource> resources = new ArrayList<>();
+    private List<Troop> troops = new ArrayList<>();
     private Location location = new Location(1, 1);
     private String type = "farm";
     private int level = 1;
@@ -42,24 +47,24 @@ public class MockApiService implements ApiService {
         };
     }
 
-        @Override
-        public Call<Building> getCertainBuilding ( @Path("userId") int userId, @Path("buildingId") int buildingId){
-            return new MockCall<Building>() {
-                @Override
-                public void enqueue(Callback callback) {
-                    callback.onResponse(null, Response.success(new Building(1, type, level, hp)));
-                }
-            };
-        }
-
-        @Override
-        public Call<Kingdom> getKingdom ( @Path("userId") final int userId){
-            return new MockCall<Kingdom>() {
-                @Override
-                public void enqueue(Callback callback) {
-                    callback.onResponse(null, Response.success(new Kingdom(id, name, idOfUser, buildings, resources, troops, location)));
-                }
-            };
-        }
+    @Override
+    public Call<Building> getCertainBuilding(@Path("userId") int userId, @Path("buildingId") int buildingId) {
+        return new MockCall<Building>() {
+            @Override
+            public void enqueue(Callback callback) {
+                callback.onResponse(null, Response.success(new Building(1, type, level, hp)));
+            }
+        };
     }
+
+    @Override
+    public Call<Kingdom> getKingdom(@Path("userId") final int userId) {
+        return new MockCall<Kingdom>() {
+            @Override
+            public void enqueue(Callback callback) {
+                callback.onResponse(null, Response.success(new Kingdom(id, name, idOfUser, buildings, resources, troops, location)));
+            }
+        };
+    }
+}
 
