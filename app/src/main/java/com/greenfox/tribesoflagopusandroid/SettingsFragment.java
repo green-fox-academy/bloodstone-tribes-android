643da09bcaa06d82b20c1a61be134843a9515bcb
georgezsiga <georgezsiga@gmail.com>
2017-06-21 15:06:37 +0200

    add switch status to java

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
@@ -5,15 +5,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 /**
  * Created by georgezsiga on 6/21/17.
  */
 
 public class SettingsFragment extends Fragment {
+
+    TextView notification_status;
+    Switch notification;
+
+    public SettingsFragment() {
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+
+        View rootView = inflater.inflate(R.layout.settings_fragment, container, false);
+
+
+        notification_status = (TextView) rootView.findViewById(R.id.notification_status);
+        notification = (Switch) rootView.findViewById(R.id.notification);
+
+        //set the switch to ON
+        notification.setChecked(true);
+        //attach a listener to check for changes in state
+        notification.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if(isChecked){
+                    notification_status.setText("Switch is currently ON");
+                }else{
+                    notification_status.setText("Switch is currently OFF");
+                }
+
+            }
+        });
+
+        //check the current state before we display the screen
+        if(notification.isChecked()){
+            notification_status.setText("Switch is currently ON");
+        }
+        else {
+            notification_status.setText("Switch is currently OFF");
+        }
+
+        return rootView;
     }
+
 }
