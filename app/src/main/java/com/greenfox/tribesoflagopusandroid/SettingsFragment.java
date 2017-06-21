435bfcc083fff068f281e73b9595bc07d84ca431
georgezsiga <georgezsiga@gmail.com>
2017-06-21 15:16:07 +0200

    add values to texts

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
@@ -27,60 +27,47 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.settings_fragment, container, false);
-
-
         notification_status = (TextView) rootView.findViewById(R.id.notification_status);
         notification = (Switch) rootView.findViewById(R.id.notification);
         background_sync_status = (TextView) rootView.findViewById(R.id.background_sync_status);
         background_sync = (Switch) rootView.findViewById(R.id.background_sync);
 
-        //set the switch to ON
-        notification.setChecked(true);
-        background_sync.setChecked(true);
-        //attach a listener to check for changes in state
         notification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-
                 if(isChecked){
-                    notification_status.setText("Switch is currently ON");
+                    notification_status.setText("You will receive game notifications");
                 }else{
-                    notification_status.setText("Switch is currently OFF");
+                    notification_status.setText("You don`t receive game notifications");
                 }
-
             }
         });
 
         background_sync.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
-
                 if(isChecked){
-                    background_sync_status.setText("Switch is currently ON");
+                    background_sync_status.setText("Background sync is ON");
                 }else{
-                    background_sync_status.setText("Switch is currently OFF");
+                    background_sync_status.setText("Background sync is OFF");
                 }
-
             }
         });
 
-        //check the current state before we display the screen
         if(notification.isChecked()){
-            notification_status.setText("Switch is currently ON");
+            notification_status.setText("You will receive game notifications");
         }
         else {
-            notification_status.setText("Switch is currently OFF");
+            notification_status.setText("You don`t receive game notifications");
         }
 
         if(background_sync.isChecked()){
-            background_sync_status.setText("Switch is currently ON");
+            background_sync_status.setText("Background sync is ON");
         }
         else {
-            background_sync_status.setText("Switch is currently OFF");
+            background_sync_status.setText("Background sync is OFF");
         }
 
         return rootView;
