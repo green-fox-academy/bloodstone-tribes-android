6e3069a25a1e92b1b1ad86400ee9354e189c6379
georgezsiga <georgezsiga@gmail.com>
2017-06-21 15:33:18 +0200

    save switch status to preferences

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.greenfox.tribesoflagopusandroid;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,22 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import javax.inject.Inject;
+
+import static com.greenfox.tribesoflagopusandroid.MainActivity.BACKGROUND_SYNC;
+import static com.greenfox.tribesoflagopusandroid.MainActivity.NOTIFICATION;
+import static com.greenfox.tribesoflagopusandroid.MainActivity.PASSWORD;
+
 /**
  * Created by georgezsiga on 6/21/17.
  */
 
 public class SettingsFragment extends Fragment {
+
+    @Inject
+    SharedPreferences preferences;
+
+    SharedPreferences.Editor editor;
 
     TextView notification_status, background_sync_status;
     Switch notification, background_sync;
@@ -25,6 +37,7 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        editor = preferences.edit();
 
         View rootView = inflater.inflate(R.layout.settings_fragment, container, false);
         notification_status = (TextView) rootView.findViewById(R.id.notification_status);
@@ -38,8 +51,12 @@ public class SettingsFragment extends Fragment {
                                          boolean isChecked) {
                 if(isChecked){
                     notification_status.setText("You will receive game notifications");
+                    editor.putBoolean(String.valueOf(NOTIFICATION), true);
+                    editor.apply();
                 }else{
                     notification_status.setText("You don`t receive game notifications");
+                    editor.putBoolean(String.valueOf(NOTIFICATION), false);
+                    editor.apply();
                 }
             }
         });
@@ -50,8 +67,12 @@ public class SettingsFragment extends Fragment {
                                          boolean isChecked) {
                 if(isChecked){
                     background_sync_status.setText("Background sync is ON");
+                    editor.putBoolean(String.valueOf(BACKGROUND_SYNC), true);
+                    editor.apply();
                 }else{
                     background_sync_status.setText("Background sync is OFF");
+                    editor.putBoolean(String.valueOf(BACKGROUND_SYNC), false);
+                    editor.apply();
                 }
             }
         });
