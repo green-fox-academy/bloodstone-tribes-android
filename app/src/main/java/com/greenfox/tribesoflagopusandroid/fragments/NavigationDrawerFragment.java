f7eb88d90a1c579264bfcbc9bb7a595909880132
Reka Giday <giday.r@gmail.com>
2017-06-21 13:59:39 +0200

    recreate navigation drawer fragment

---
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/fragments/NavigationDrawerFragment.java
@@ -0,0 +1,25 @@
+package com.greenfox.tribesoflagopusandroid.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.greenfox.tribesoflagopusandroid.R;
+
+public class NavigationDrawerFragment extends Fragment {
+
+
+    public NavigationDrawerFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+}
