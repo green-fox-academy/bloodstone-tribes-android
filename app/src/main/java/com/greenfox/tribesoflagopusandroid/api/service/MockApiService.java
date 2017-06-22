a5fa08a2acddcf3ddb21ae8f327a1ccf8824b046
Reka Giday <giday.r@gmail.com>
2017-06-22 14:53:17 +0200

    dummy lists filled

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/MockApiService.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/MockApiService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,11 +27,15 @@ public class MockApiService implements ApiService {
     private String name = "kingdomname";
     private long idOfUser = 1;
     private Building farm1 = new Building(1, "farm", 1, 2);
-    private Building mine1 = new Building(1, "mine", 1, 2);
-    private Building townhall = new Building(1, "townhall", 1, 10);
+    private Building mine1 = new Building(2, "mine", 1, 2);
+    private Building townhall = new Building(3, "townhall", 1, 10);
     private List<Building> buildings = new ArrayList<>(Arrays.asList(farm1, mine1, townhall));
-    private List<Resource> resources = new ArrayList<>();
-    private List<Troop> troops = new ArrayList<>();
+    private Resource gold = new Resource("gold", 10, 1);
+    private Resource food = new Resource("food", 20, 1);
+    private List<Resource> resources = new ArrayList<>(Arrays.asList(gold, food));
+    private Troop troop1 = new Troop(1, 1, 10, 1, 2);
+    private Troop troop2 = new Troop(2, 1, 10, 2, 2);
+    private List<Troop> troops = new ArrayList<>(Arrays.asList(troop1, troop2));
     private Location location = new Location(1, 1);
     private String type = "farm";
     private int level = 1;
