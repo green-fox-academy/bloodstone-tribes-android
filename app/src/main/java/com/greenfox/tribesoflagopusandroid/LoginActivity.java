ea93312241bf1290e4f833571228299754b9af6d
georgezsiga <georgezsiga@gmail.com>
2017-06-26 16:27:53 +0200

    add welcome notification

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.greenfox.tribesoflagopusandroid;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Gravity;
@@ -12,7 +15,6 @@ import android.widget.Toast;
 
 import com.greenfox.tribesoflagopusandroid.api.model.gameobject.User;
 import com.greenfox.tribesoflagopusandroid.api.service.LoginService;
-import com.greenfox.tribesoflagopusandroid.api.service.ServiceFactory;
 
 import javax.inject.Inject;
 
@@ -25,9 +27,12 @@ import static com.greenfox.tribesoflagopusandroid.MainActivity.USERNAME;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Inject SharedPreferences preferences;
-    @Inject ObjectManager objectManager;
-    @Inject LoginService loginService;
+    @Inject
+    SharedPreferences preferences;
+    @Inject
+    ObjectManager objectManager;
+    @Inject
+    LoginService loginService;
 
     SharedPreferences.Editor editor;
 
@@ -45,14 +50,14 @@ public class LoginActivity extends AppCompatActivity {
         String username = ((EditText) findViewById(R.id.usernameText)).getText().toString();
         String password = ((EditText) findViewById(R.id.passwordText)).getText().toString();
 
-        checkFieldsNotEmpty(username, password);
+        checkFieldsNotEmpty(username, password, view);
     }
 
-    public void checkFieldsNotEmpty(String username, String password) {
+    public void checkFieldsNotEmpty(String username, String password, View view) {
         if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
             showToastFillAllFields();
         } else {
-            loginWithAPIService(username, password);
+            loginWithAPIService(username, password, view);
         }
     }
 
@@ -62,20 +67,33 @@ public class LoginActivity extends AppCompatActivity {
         toast.show();
     }
 
-    protected void loginWithAPIService(String username, String password) {
+    protected void loginWithAPIService(String username, String password, final View view) {
         loginService.loginWithUser(username, password).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 User user = response.body();
                 addUserInfoToPreferences(user.getUsername());
+                sendNotification(user.getUsername(), view);
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
+
             @Override
             public void onFailure(Call<User> call, Throwable t) {
                 Toast.makeText(LoginActivity.this, "error getting login information from server", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void sendNotification(String username, View view) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.tribes)
+                        .setContentTitle("Hi " + username + "!")
+                        .setContentText("Welcome to the game, and have fun!");
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(001, mBuilder.build());
     }
 
     protected void addUserInfoToPreferences(String username) {
