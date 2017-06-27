f79226d585299f754f74de7845d16c565b0ffbf9
georgezsiga <georgezsiga@gmail.com>
2017-06-27 14:35:06 +0200

    save state on exit

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/fragments/MainFragment.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/fragments/MainFragment.java
@@ -1,6 +1,7 @@
 package com.greenfox.tribesoflagopusandroid.fragments;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,9 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.greenfox.tribesoflagopusandroid.R;
+import com.greenfox.tribesoflagopusandroid.TribesApplication;
+
+import javax.inject.Inject;
+
+import static com.greenfox.tribesoflagopusandroid.MainActivity.MAIN_FRAGMENT_SAVE;
 
 public class MainFragment extends Fragment {
 
+    @Inject
+    SharedPreferences preferences;
+
+    SharedPreferences.Editor editor;
+
+    String timestamp;
 
     public MainFragment() {
     }
@@ -19,8 +31,17 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        TribesApplication.app().basicComponent().inject(this);
+        editor = preferences.edit();
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        timestamp = String.valueOf(System.currentTimeMillis());
+        editor.putString(MAIN_FRAGMENT_SAVE, timestamp);
+        editor.apply();
     }
 
 }
