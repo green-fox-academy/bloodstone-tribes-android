20f83bb991ffb36be926a7083cd8c30e67b3fb41
georgezsiga <georgezsiga@gmail.com>
2017-06-28 15:38:28 +0200

    add basefragment

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/BasicComponent.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/BasicComponent.java
@@ -1,5 +1,6 @@
 package com.greenfox.tribesoflagopusandroid;
 
+import com.greenfox.tribesoflagopusandroid.fragments.BaseFragment;
 import com.greenfox.tribesoflagopusandroid.fragments.BuildingsFragment;
 import com.greenfox.tribesoflagopusandroid.fragments.MainFragment;
 import com.greenfox.tribesoflagopusandroid.fragments.SettingsFragment;
@@ -22,4 +23,5 @@ public interface BasicComponent {
     void inject(MainFragment fragment);
     void inject(BuildingsFragment fragment);
     void inject(TroopsFragment fragment);
+    void inject(BaseFragment fragment);
 }
