9999504872c8e956b0343f0a6a76a14da685edc8
Reka Giday <giday.r@gmail.com>
2017-06-28 11:41:55 +0200

    add drawertests to mainactivity tests too

--- app/src/test/java/com/greenfox/tribesoflagopusandroid/MainActivityTest.java
+++ app/src/test/java/com/greenfox/tribesoflagopusandroid/MainActivityTest.java
@@ -1,13 +1,18 @@
 package com.greenfox.tribesoflagopusandroid;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import static com.greenfox.tribesoflagopusandroid.MainActivity.USERNAME;
@@ -20,13 +25,21 @@ import static org.robolectric.Shadows.shadowOf;
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
+    private static final int MP = ViewGroup.LayoutParams.MATCH_PARENT;
+
     MainActivity main;
     LoginActivity login;
+    DrawerLayout drawerLayout;
 
     @Before
     public void setup() {
         main = Robolectric.setupActivity(MainActivity.class);
         login = Robolectric.setupActivity(LoginActivity.class);
+        drawerLayout = new DrawerLayout(RuntimeEnvironment.application);
+        drawerLayout.addView(
+                new TextView(RuntimeEnvironment.application),
+                0,
+                new DrawerLayout.LayoutParams(MP, MP, GravityCompat.START));
     }
 
     @Test
@@ -36,4 +49,22 @@ public class MainActivityTest {
         Intent expectedIntent = new Intent(login, LoginActivity.class);
         assertEquals(expectedIntent.getClass(), shadowOf(main).getNextStartedActivity().getClass());
     }
+
+    @Test
+    public void testDrawerIsClosedByDefault() throws Exception {
+        assertThat(drawerLayout.isDrawerOpen(GravityCompat.START), is(false));
+    }
+
+    @Test
+    public void testDrawerOpens() throws Exception {
+        drawerLayout.openDrawer(GravityCompat.START);
+        assertThat(drawerLayout.isDrawerOpen(GravityCompat.START), is(true));
+    }
+
+//    @Test
+//    public void testBuildingsMenuClickedRedirectsToBuildings() throws Exception {
+//        drawerLayout.openDrawer(GravityCompat.START);
+//        drawerLayout.findViewById(R.id.nav_buildings).performClick();
+//        assertEquals("Buildings", main.getTitle().toString());
+//    }
 }
