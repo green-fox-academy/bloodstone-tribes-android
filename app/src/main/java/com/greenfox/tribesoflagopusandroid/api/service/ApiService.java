e6e48b6f89745524fde0a7c1a61b16f21bc8edcf
georgezsiga <georgezsiga@gmail.com>
2017-06-29 14:32:03 +0200

    delete user id

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/ApiService.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/api/service/ApiService.java
@@ -1,17 +1,16 @@
 package com.greenfox.tribesoflagopusandroid.api.service;
 
-import com.greenfox.tribesoflagopusandroid.api.model.response.TroopsResponse;
 import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Building;
 import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Kingdom;
-import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Resource;
 import com.greenfox.tribesoflagopusandroid.api.model.response.BuildingsResponse;
 import com.greenfox.tribesoflagopusandroid.api.model.response.ResourcesResponse;
+import com.greenfox.tribesoflagopusandroid.api.model.response.TroopsResponse;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Field;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * Created by hegyi on 2017-06-22.
@@ -19,22 +18,22 @@ import retrofit2.http.POST;
 
 public interface ApiService {
 
-    @GET("/{userId}/kingdom/troops")
-    Call<TroopsResponse> getTroops(@Path("userId") int userId);
+    @GET("/kingdom/troops")
+    Call<TroopsResponse> getTroops();
 
-    @GET("/{userId}/kingdom/buildings")
-    Call<BuildingsResponse> getBuildings(@Path("userId") int userId);
+    @GET("/kingdom/buildings")
+    Call<BuildingsResponse> getBuildings();
 
-    @GET("/{userId}/kingdom/buildings/{buildingId}")
-    Call<Building> getCertainBuilding(@Path("userId") int userId, @Path("buildingId")int buildingId);
+    @GET("/kingdom/buildings/{buildingId}")
+    Call<Building> getCertainBuilding(@Path("buildingId")int buildingId);
 
-    @GET("/{userId}/kingdom")
-    Call<Kingdom> getKingdom(@Path("userId") int userId);
+    @GET("/kingdom")
+    Call<Kingdom> getKingdom();
 
-    @POST("/{userId}/kingdom/buildings")
-    Call<Building> postBuilding (@Field("type") String type);
+    @POST("/kingdom/buildings")
+    Call<Building> postBuilding(@Field("type") String type);
 
-    @GET("/{userId}/kingdom/resources")
-    Call<ResourcesResponse> getResource (@Path("userId") int userId);
+    @GET("/kingdom/resources")
+    Call<ResourcesResponse> getResource();
 
 }
