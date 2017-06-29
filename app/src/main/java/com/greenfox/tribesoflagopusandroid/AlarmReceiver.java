f9b53f56f636ed0e79255703a88d1faede2d2983
georgezsiga <georgezsiga@gmail.com>
2017-06-29 13:23:43 +0200

    call api service at every alarm

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/AlarmReceiver.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/AlarmReceiver.java
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.greenfox.tribesoflagopusandroid.adapter.KingdomAdapter;
-import com.greenfox.tribesoflagopusandroid.api.model.response.KingdomResponse;
+import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Kingdom;
 import com.greenfox.tribesoflagopusandroid.api.service.ApiService;
 
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by georgezsiga on 6/27/17.
@@ -16,27 +19,26 @@ import javax.inject.Inject;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    private KingdomAdapter kingdomAdapter;
     @Inject
     ApiService apiService;
+
+    Kingdom thisKingdom = new Kingdom();
 
     @Override
     public void onReceive(Context arg0, Intent arg1) {
         TribesApplication.app().basicComponent().inject(this);
 
-        apiService.getKingdom(1).enqueue(new Callback<KingdomResponse>() {
-
+        apiService.getKingdom(1).enqueue(new Callback<Kingdom>() {
             @Override
-            public void onResponse(Call<KingdomResponse> call, Response<KingdomResponse> response) {
-
+            public void onResponse(Call<Kingdom> call, Response<Kingdom> response) {
+                thisKingdom = response.body();
             }
 
             @Override
-            public void onFailure(Call<KingdomResponse> call, Throwable t) {
+            public void onFailure(Call<Kingdom> call, Throwable t) {
 
             }
         });
-        kingdomAdapter = new KingdomAdapter(getContext(), new ArrayList<Troop>());
-
     }
+
 }
