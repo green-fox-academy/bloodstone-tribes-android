2232330f7aa26d3451d19a2064a7374227c82d47
Reka Giday <giday.r@gmail.com>
2017-06-30 11:26:18 +0200

    removed extra lines

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/fragments/TroopsFragment.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/fragments/TroopsFragment.java
@@ -90,7 +90,7 @@ public class TroopsFragment extends BaseFragment {
                 });
             }
         });
-
+        
         ListView listView = (ListView) rootView.findViewById(R.id.troops_listView);
         listView.setAdapter(troopAdapter);
 
