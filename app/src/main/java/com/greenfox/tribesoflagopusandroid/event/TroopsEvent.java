f5254ac7d540e3d06be4d23de43576133b0f1b96
Reka Giday <giday.r@gmail.com>
2017-06-30 09:40:27 +0200

    change troopslist type to troops

--- app/src/main/java/com/greenfox/tribesoflagopusandroid/event/TroopsEvent.java
+++ app/src/main/java/com/greenfox/tribesoflagopusandroid/event/TroopsEvent.java
@@ -3,6 +3,7 @@ package com.greenfox.tribesoflagopusandroid.event;
 import com.greenfox.tribesoflagopusandroid.api.model.gameobject.Troop;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by User on 2017. 06. 28..
@@ -10,9 +11,20 @@ import java.util.ArrayList;
 
 public class TroopsEvent {
 
-    ArrayList<Troop> troops;
+    List<Troop> troops;
 
-    public TroopsEvent(ArrayList<Troop> troops) {
+    public TroopsEvent() {
+    }
+
+    public TroopsEvent(List<Troop> troops) {
+        this.troops = troops;
+    }
+
+    public List<Troop> getTroops() {
+        return troops;
+    }
+
+    public void setTroops(List<Troop> troops) {
         this.troops = troops;
     }
 }
